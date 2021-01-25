02a113071df91f1109a7162a33a0c8ec8ae94650
976047167 <976047167@qq.com>
2021-01-25 18:16:59 +0800

    改名，避免歧义

--- Assets/Scripts/GameActions/StartMove.cs
+++ Assets/Scripts/GameActions/StartMove.cs
@@ -63,72 +63,72 @@ namespace GameAction
 			// The start index is based on the input value.
 			Vector2 inputValue = this.ownerLocomotion.GetInputVector();
 			float speed = Constants.SpeedAcceleration;
-			int AnimationIndex = (int)StartIndex.None;
+			int moveArg = (int)StartIndex.None;
 			if (inputValue.x > speed && inputValue.y > speed)
 			{
-				AnimationIndex = (int)StartIndex.RunForwardTurnRight;
+				moveArg = (int)StartIndex.RunForwardTurnRight;
 			}
 			else if (inputValue.x > 0 && inputValue.y > 0)
 			{
-				AnimationIndex = (int)StartIndex.WalkForwardTurnRight;
+				moveArg = (int)StartIndex.WalkForwardTurnRight;
 			}
 			else if (inputValue.x < -speed && inputValue.y > speed)
 			{
-				AnimationIndex = (int)StartIndex.RunForwardTurnLeft;
+				moveArg = (int)StartIndex.RunForwardTurnLeft;
 			}
 			else if (inputValue.x < 0 && inputValue.y > 0)
 			{
-				AnimationIndex = (int)StartIndex.WalkForwardTurnLeft;
+				moveArg = (int)StartIndex.WalkForwardTurnLeft;
 			}
 			else if (inputValue.x < -speed && inputValue.y < -speed)
 			{
-				AnimationIndex = (int)StartIndex.RunBackwardTurnLeft;
+				moveArg = (int)StartIndex.RunBackwardTurnLeft;
 			}
 			else if (inputValue.x < 0 && inputValue.y < 0)
 			{
-				AnimationIndex = (int)StartIndex.WalkBackwardTurnLeft;
+				moveArg = (int)StartIndex.WalkBackwardTurnLeft;
 			}
 			else if (inputValue.x > speed && inputValue.y < -speed)
 			{
-				AnimationIndex = (int)StartIndex.RunBackwardTurnRight;
+				moveArg = (int)StartIndex.RunBackwardTurnRight;
 			}
 			else if (inputValue.x > 0 && inputValue.y < 0)
 			{
-				AnimationIndex = (int)StartIndex.WalkBackwardTurnRight;
+				moveArg = (int)StartIndex.WalkBackwardTurnRight;
 			}
 			else if (inputValue.y > speed)
 			{
-				AnimationIndex = (int)StartIndex.RunForward;
+				moveArg = (int)StartIndex.RunForward;
 			}
 			else if (inputValue.y > 0)
 			{
-				AnimationIndex = (int)StartIndex.WalkForward;
+				moveArg = (int)StartIndex.WalkForward;
 			}
 			else if (inputValue.y < -speed)
 			{
-				AnimationIndex = (int)StartIndex.RunBackward;
+				moveArg = (int)StartIndex.RunBackward;
 			}
 			else if (inputValue.y < 0)
 			{
-				AnimationIndex = (int)StartIndex.WalkBackward;
+				moveArg = (int)StartIndex.WalkBackward;
 			}
 			else if (inputValue.x > speed)
 			{
-				AnimationIndex = (int)StartIndex.RunStrafeRight;
+				moveArg = (int)StartIndex.RunStrafeRight;
 			}
 			else if (inputValue.x > 0)
 			{
-				AnimationIndex = (int)StartIndex.WalkStrafeRight;
+				moveArg = (int)StartIndex.WalkStrafeRight;
 			}
 			else if (inputValue.x < -speed)
 			{
-				AnimationIndex = (int)StartIndex.RunStrafeLeft;
+				moveArg = (int)StartIndex.RunStrafeLeft;
 			}
 			else if (inputValue.x < 0)
 			{
-				AnimationIndex = (int)StartIndex.WalkStrafeLeft;
+				moveArg = (int)StartIndex.WalkStrafeLeft;
 			}
-			this.AnimatorInt = AnimationIndex;
+			this.AnimatorInt = moveArg;
 
 			base.Activavte();
 		}
